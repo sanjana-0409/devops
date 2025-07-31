80bf5e71eea6840c881fb4df53c167afbdf05f1a
sanjana-0409 <146005023+sanjana-0409@users.noreply.github.com>
2025-07-31 14:54:59 +0530

    final comit

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 public class sample {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("SCM polling to monitor change commits in git hub!");
     }
 }
